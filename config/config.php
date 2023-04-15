af6b6f69c739c66b245593a80405044748646ab2
agungyuni12 <110798925+agungyuni12@users.noreply.github.com>
2023-04-15 15:40:15 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "n1607753_emalia";
-$password = "emalia12345678";
-$database = "n1607753_survei_nwas";
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
 // Create connection
 
 $conn = mysqli_connect($servername, $username, $password, $database);
